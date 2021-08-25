970bdfb7149cdea3c8795e51bf10cb66eb3abb82
Sebastian <jnavarreterodriguez19@gmail.com>
2021-08-25 00:05:11 -0500

    arreglo en el cors

--- app/settings/production.py
+++ app/settings/production.py
@@ -31,12 +31,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3000/checkout",
     "http://localhost:3000/api",
-    "https://miguesc2.github.io/",
+    "https://miguesc2.github.io",
 ]
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://localhost:3000/checkout",
     "http://localhost:3000/api",
-    "https://miguesc2.github.io/",
+    "https://miguesc2.github.io",
 ]
