6eb1c8dba2cfb0debced13cd9f64089b7dc85c05
Sebastian <jnavarreterodriguez19@gmail.com>
2021-08-27 22:01:09 -0500

    agregada ruta para el login

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ from applications.accounts.views.login_view import Login, Logout, UserToken
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('applications.accounts.urls')),
-    path('', Login.as_view(), name = 'Login'),
+    path('login/', Login.as_view(), name = 'Login'),
     path('logout/', Logout.as_view(), name = 'Logout'),
     path('refresh-token/', UserToken.as_view(), name = 'refresh_token'),
 ]
